b5a5259f52ceaf02f5071eefbdc96b9a28efaa90
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2020-09-04 19:49:04 +0300

    Double linked list

--- render_mask.h
+++ render_mask.h
@@ -1,7 +1,11 @@
 #ifndef RENDER_MASK_H
 #define RENDER_MASK_H
 
-#include <CL/cl.h>
+#ifdef __APPLE__
+# include <OpenCL/cl.h>
+#elif __linux__
+# include <CL/cl.h>
+#endif
 
 char *create_render_mask(const size_t size, const int pobability_black);
 
